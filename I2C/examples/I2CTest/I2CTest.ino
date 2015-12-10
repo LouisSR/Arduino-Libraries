1afcf7b4a1f58c279526ec7f47127c882d5f4969
Louis Saint-Raymond <louis.saint-raymond@sfu.ca>
2015-12-10 20:18:29 +0100

    Added sending of messages

--- I2C/examples/I2CTest/I2CTest.ino
+++ I2C/examples/I2CTest/I2CTest.ino
@@ -4,26 +4,40 @@
 #define SLAVE_ADDRESS 0x12
 
 uint8_t messageReceived[COM_IN_LENGTH];
+uint8_t message2send[]={0,0};
+
+uint8_t iter = 0;
 
 void setup() 
 {
 	i2cBegin(SLAVE_ADDRESS);
 	Serial.begin(19200);
 	Serial.println("I2C Test ");
+	i2cSetMessage(message2send);
 }
 
 void loop() 
 {
-	delay(100);
-	if( 1 )
+	boolean new_message = 0;
+
+	new_message = i2cGetMessage(messageReceived);
+
+	if( new_message )
 	{
-		i2cGetMessage(messageReceived);
+		//Display message received
 		Serial.print("Message: ");
-		for(int i = 0;i<COM_IN_LENGTH-3; i++)
+		for(int i = 0;i<COM_IN_LENGTH-COM_OVERHEAD; i++)
 		{
 			Serial.print(messageReceived[i]);
 			Serial.print(" ");
 		}
 		Serial.println();
+
+		//Update message to send
+		message2send[0] = 76 - iter;
+		message2send[1] = 105 + iter;
+		i2cSetMessage(message2send);
+		iter++;
 	}
+	delay(100);
 }
