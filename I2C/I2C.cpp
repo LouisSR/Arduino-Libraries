a266e15af1a9a6ad0ff406555023318079409261
LouisSR <louis.internet.online@gmail.com>
2016-05-28 21:13:53 +0200

    Removing checksum, modifying message length

--- I2C/I2C.cpp
+++ I2C/I2C.cpp
@@ -10,7 +10,7 @@ uint8_t i2cChecksum(uint8_t* data, uint8_t length);
 
 uint8_t message_to_send[COM_OUT_LENGTH-COM_OVERHEAD];
 uint8_t dataReceived[COM_IN_LENGTH];
-boolean _new_message;
+bool _new_message;
 
 
 void i2cBegin(uint8_t slaveAdress)
@@ -28,13 +28,11 @@ void i2cBegin(uint8_t slaveAdress)
 
 boolean i2cGetMessage(uint8_t* message)
 {
-
 	if(_new_message)
 	{
 		//Todo: verify head, length and checksum
 		uint8_t command = dataReceived[0];
 		uint8_t data_length = dataReceived[1];
-		uint8_t checksum = dataReceived[data_length+2];
 
 		//Collect data
 		for (uint8_t i=0;i<data_length;i++)
@@ -63,7 +61,6 @@ void i2cSendData()
 	uint8_t data_to_send[COM_OUT_LENGTH];
 	data_to_send[0] = START_BYTE;
 	data_to_send[1] = COM_OUT_LENGTH-COM_OVERHEAD;
-	data_to_send[COM_OUT_LENGTH-1] = i2cChecksum(message_to_send, COM_OUT_LENGTH-COM_OVERHEAD);
 	for(uint8_t i=0; i<COM_OUT_LENGTH-COM_OVERHEAD; i++)
 	{
 		data_to_send[i+2]=message_to_send[i];
@@ -86,11 +83,6 @@ void i2cReceiveData(int byteCount)
 	{
 		Wire.read();
 	}
-}
-
-uint8_t i2cChecksum(uint8_t* data, uint8_t length)
-{
-	return 255;
 }
 
 
--- I2C/I2C.h
+++ I2C/I2C.h
@@ -9,14 +9,14 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#define COM_IN_LENGTH 6 // Length of I2C recieved data
-#define COM_OUT_LENGTH 5 // Length of I2C sent data
-#define COM_OVERHEAD 3
+#define COM_IN_LENGTH 5 // Length of I2C recieved data
+#define COM_OUT_LENGTH 10 // Length of I2C sent data
+#define COM_OVERHEAD 2
 #define START_BYTE 15
-
+	
 
 void i2cBegin(uint8_t slaveAdress);
-boolean i2cGetMessage(uint8_t* message);
+bool i2cGetMessage(uint8_t* message);
 void i2cSetMessage(uint8_t* message);
 void i2cSendData();
 void i2cReceiveData(int byteCount);
